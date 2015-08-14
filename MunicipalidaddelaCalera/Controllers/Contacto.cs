bba912998832c047adce7d738db139a1672c2f1f
VeronicaLanda <landaveronica@hotmail.com>
2015-08-13 21:41:19 -0300

    controladores

---
+++ MunicipalidaddelaCalera/Controllers/Contacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MunicipalidaddelaCalera.Controllers
+{
+    public class Contacto : Controller
+    {
+        // GET: Contacti
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
