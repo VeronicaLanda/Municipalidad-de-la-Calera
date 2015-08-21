c89d4c0fef50959350f49bf91a78dd6fdfd26a79
aleediaaz03 <ale.diaz.9595@gmail.com>
2015-08-21 19:52:00 -0300

    Add User

---
+++ MunicipalidaddelaCalera/Models/Catergory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MunicipalidaddelaCalera.Models
+{
+    public class Catergory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- MunicipalidaddelaCalera/Models/IdentityModels.cs
+++ MunicipalidaddelaCalera/Models/IdentityModels.cs
@@ -20,6 +20,10 @@ namespace MunicipalidaddelaCalera.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Catergory> Categories { get; set; }
+
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
